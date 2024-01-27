767fdc7cfa4e4d46b9d3ed98f89919d84fc7e80f
Vilhelm <vilhelm.lindell123@gmail.com>
2024-01-27 22:13:16 +0100

    added support for all days

--- rooms.js
+++ rooms.js
@@ -45,23 +45,24 @@ async function getFreeRooms() {
     let response = await fetch('lesson_times.json');
     let lessonTimes = await response.json();
 
-    //let current_time = getCurrentTime();
-    let current_time = "14:15:00";
+    let current_time = getCurrentTime();
+    //let current_time = "14:15:00";
     let free_rooms = [];
     let occupied_rooms = [];
 
     for (let room_lesson_times of lessonTimes) {
         let room = room_lesson_times[0];
+        let day = Math.min(new Date().getDay(), 4);
 
-        for (let i = 0; i < room_lesson_times[1].length; i++) {
-            let lesson_time = room_lesson_times[1][i];
+        for (let i = 0; i < room_lesson_times[1][day].length; i++) {
+            let lesson_time = room_lesson_times[1][day][i];
 
             if (current_time <= lesson_time[0]) {
                 let free_duration = timeDifference(current_time, lesson_time[0]);
                 free_rooms.push([room, free_duration]);
                 break;
             } else if (current_time >= lesson_time[1]) {
-                if (i === room_lesson_times[1].length - 1) {
+                if (i === room_lesson_times[1][day].length - 1) {
                     let free_duration = "Resten av dagen";
                     free_rooms.push([room, free_duration]);
                     break;
@@ -69,8 +70,8 @@ async function getFreeRooms() {
                 continue;
             } else if (current_time >= lesson_time[0]) {
                 let free_duration;
-                if (i < room_lesson_times[1].length - 1) {
-                    let next_lesson_time = room_lesson_times[1][i + 1];
+                if (i < room_lesson_times[1][day].length - 1) {
+                    let next_lesson_time = room_lesson_times[1][day][i + 1];
                     free_duration = timeDifference(lesson_time[1], next_lesson_time[0]);
                 } else {
                     free_duration = "Resten av dagen";
@@ -85,17 +86,17 @@ async function getFreeRooms() {
         }
 
     }
-        free_rooms.sort((a, b) => b[1].localeCompare(a[1]));
-        occupied_rooms.sort((a, b) => a[1].localeCompare(b[1]) || b[2].localeCompare(a[2]));
 
-        let freeRoomsElement = document.createElement('table');
-        freeRoomsElement.innerHTML = '<tr><th>Lediga Rum</th><th>Tid Ledigt</th></tr>' + free_rooms.map(room => `<tr class="free"><td>${room[0]}</td><td>${room[1]}</td></tr>`).join('');
-        document.body.appendChild(freeRoomsElement);
+    free_rooms.sort((a, b) => b[1].localeCompare(a[1]));
+    occupied_rooms.sort((a, b) => a[1].localeCompare(b[1]) || b[2].localeCompare(a[2]));
 
-        let occupiedRoomsElement = document.createElement('table');
-        occupiedRoomsElement.innerHTML = '<tr><th>Upptagna Rum</th><th>Ledigt Om</th><th>Tid Ledigt</th></tr>' + occupied_rooms.map(room => `<tr class="occupied"><td>${room[0]}</td><td>${room[1]}</td><td>${room[2]}</td></tr>`).join('');
-        document.body.appendChild(occupiedRoomsElement);
+    let freeRoomsElement = document.createElement('table');
+    freeRoomsElement.innerHTML = '<tr><th>Lediga Rum Spyken</th><th>Tid Ledigt</th></tr>' + free_rooms.map(room => `<tr class="free"><td>${room[0]}</td><td>${room[1]}</td></tr>`).join('');
+    document.body.appendChild(freeRoomsElement);
 
+    let occupiedRoomsElement = document.createElement('table');
+    occupiedRoomsElement.innerHTML = '<tr><th>Upptagna Rum</th><th>Ledigt Om</th><th>Tid Ledigt</th></tr>' + occupied_rooms.map(room => `<tr class="occupied"><td>${room[0]}</td><td>${room[1]}</td><td>${room[2]}</td></tr>`).join('');
+    document.body.appendChild(occupiedRoomsElement);
 }
 
 getFreeRooms();
